3e472333df8d002bcede78b9a4c520ca7fb54f4f
Dominik <ramirez459@gmail.com>
2019-12-25 22:42:54 +0100

    Database connection
      + HibernateConf
      + Project structure
      + Connection to the database

---
+++ src/main/java/com/tai/vikopru/datasource/HibernateConf.java
@@ -0,0 +1,42 @@
+package com.tai.vikopru.datasource;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+public class HibernateConf {
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.tai.vikopru.entity" );
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
+
+    @Bean
+    @ConfigurationProperties("app.datasource")
+    public HikariDataSource dataSource() {
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    private Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty(
+                "hibernate.hbm2ddl.auto", "create-drop");
+        hibernateProperties.setProperty(
+                "hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        return hibernateProperties;
+    }
+}
