9515c2a2bf76eff9ce0aa512f8685d77bcce361b
Dominik <ramirez459@gmail.com>
2019-12-25 22:25:16 +0100

    spring boot

---
+++ src/main/java/com/tai/vikopru/VikopruApplication.java
@@ -0,0 +1,13 @@
+package com.tai.vikopru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VikopruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VikopruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tai/vikopru/VikopruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tai.vikopru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VikopruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
