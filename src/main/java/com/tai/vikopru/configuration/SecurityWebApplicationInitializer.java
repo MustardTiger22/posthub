3280a9d1451e54233134961e5066da081a8ec54a
Dominik <ramirez459@gmail.com>
2019-12-26 22:25:38 +0100

    Added spring security dependencies and configuration class
      + Configuration/SecurityConf

---
+++ src/main/java/com/tai/vikopru/configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,9 @@
+package com.tai.vikopru.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
